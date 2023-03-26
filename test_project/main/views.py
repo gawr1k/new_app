615baf49019ab6cfabfb8341187b97008a0c3628
gawr1k <gasimmurasov@gmail.com>
2023-03-26 20:28:30 +0500

    added the shoblanizer

--- test_project/main/views.py
+++ test_project/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h4>Проверка работы</h4>")
+    return render(request, "main/index.html")
+
+def about(request):
+    return render(request, "main/about.html")
+
